a500937045438b75b6acecb91e37efb60a043978
hoyul <food030921@hanmail.net>
2024-02-11 21:14:26 +0900

    init: create springboot project
    - springboot: 3.2.2
    - java: 17
    - build gradle

---
+++ munchmap/src/main/java/com/ggoomho/munchmap/MunchmapApplication.java
@@ -0,0 +1,13 @@
+package com.ggoomho.munchmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchmapApplication.class, args);
+	}
+
+}
---
+++ munchmap/src/test/java/com/ggoomho/munchmap/MunchmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggoomho.munchmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
